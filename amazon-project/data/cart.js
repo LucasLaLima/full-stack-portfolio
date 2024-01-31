934c3b1bab3a3ca9a1bf3c4ce21218bba87dc274
Lucas LaLima <luke@Lucass-MacBook-Pro.local>
2024-01-30 23:26:36 -0500

    Updates delivery date on checkout page

--- amazon-project/data/cart.js
+++ amazon-project/data/cart.js
@@ -7,11 +7,13 @@ if (!cart) {
   [
     {
       productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      quantity: 1
+      quantity: 1,
+      deliveryOptionId: '1'
     },
     {
       productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-      quantity: 20
+      quantity: 10,
+      deliveryOptionId: '2'
     }
   ];
 }
@@ -66,7 +68,8 @@ export function addToCart(productId) {
       // quantity: quantityValue
       // ---
       productId,
-      quantity
+      quantity,
+      deliveryOptionId: '1'
     });
   }
 
